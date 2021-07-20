5d0eb67d4ac762b0576ed35559770f979eda1120
Scott Duda <scottmduda@gmail.com>
2021-07-19 20:55:29 -0400

    initial commit

---
+++ django_pitt_crash/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from django_pitt_crash.users.models import User
+from django_pitt_crash.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
